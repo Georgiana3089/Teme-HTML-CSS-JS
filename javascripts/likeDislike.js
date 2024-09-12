bd734793d76a4acbc977cc6fa3f70f499942bca7
Doruta Georgiana Chinta <166866124+Georgiana3089@users.noreply.github.com>
2024-09-12 22:21:24 +0300

    Delete javascripts directory

--- javascripts/likeDislike.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const ui = {
-  containerLikeDislike: document.querySelector("#containerLikeDislike"),
-  likesCounter: document.querySelector(".like-count"),
-  dislikesCounter: document.querySelector(".dislike-count"),
-  likeButton: document.querySelector(".click-like"),
-  dislikeButton: document.querySelector(".click-dislike"),
-};
-
